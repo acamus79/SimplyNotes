025a9a5edc6cba12935385727926d3771a379588
Adrian Edgardo Camus <adrianedgardocamus@gmail.com>
2023-09-02 11:23:24 -0300

    Update INoteRepository.java

--- backend/SimplyNotes/src/main/java/com/aec/simplynotes/repositories/INoteRepository.java
+++ backend/SimplyNotes/src/main/java/com/aec/simplynotes/repositories/INoteRepository.java
@@ -2,8 +2,23 @@ package com.aec.simplynotes.repositories;
 
 import com.aec.simplynotes.models.NoteEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface INoteRepository  extends JpaRepository<NoteEntity, String> {
+
+    /**
+     * Method to search only the records with the softDelete field set to false,
+     *
+     * @return List(Entity)
+     */
+    @Query("SELECT n FROM NoteEntity n WHERE n.softDelete = false")
+    public List<NoteEntity> searchAllNonDeleted();
+
+    @Query("SELECT n FROM NoteEntity n WHERE n.user = :id AND n.softDelete = false")
+    public List<NoteEntity> searchAllNonDeletedByUserId(String id);
+
 }
