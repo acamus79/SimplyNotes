eda0882f79f94b90db34ca9ada61a04f6f0ae896
Adrian Edgardo Camus <adrianedgardocamus@gmail.com>
2023-09-01 22:23:17 -0300

    Create NoteEntity.java

---
+++ backend/SimplyNotes/src/main/java/com/aec/simplynotes/models/NoteEntity.java
@@ -0,0 +1,49 @@
+package com.aec.simplynotes.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+@Entity
+@Table(name = "activities")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@SQLDelete(sql = "UPDATE activities SET soft_delete = true WHERE id=?")
+@Where(clause = "soft_delete = false")
+@EntityListeners(AuditingEntityListener.class)
+public class NoteEntity implements Serializable{
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    @GeneratedValue(generator = "uuid")
+    @Column(length = 36)
+    private String id;
+
+    @Column(nullable = false, length = 60)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private Boolean archived = Boolean.FALSE;
+
+    @Column(nullable = false)
+    @CreatedDate
+    private LocalDateTime timestamps;
+
+    @Column(nullable = false)
+    private Boolean softDelete = Boolean.FALSE;
+
+}
