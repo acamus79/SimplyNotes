3f16d5f957415e40309ae98743667d7ecea5d5c9
Adrian Edgardo Camus <adrianedgardocamus@gmail.com>
2023-09-02 11:23:09 -0300

    Update NoteController.java

--- backend/SimplyNotes/src/main/java/com/aec/simplynotes/controllers/NoteController.java
+++ backend/SimplyNotes/src/main/java/com/aec/simplynotes/controllers/NoteController.java
@@ -4,8 +4,12 @@ import com.aec.simplynotes.dto.request.EntryNoteDto;
 import com.aec.simplynotes.dto.response.NoteDto;
 import com.aec.simplynotes.exeptions.ValidationException;
 import com.aec.simplynotes.services.NoteService;
+import io.swagger.v3.oas.annotations.Parameter;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -43,7 +47,7 @@ public class NoteController {
      * @param errors Validation errors (if any).
      * @return An HTTP response with the updated note in the body.
      */
-    @PutMapping("/api/notes/{id}")
+    @PutMapping("/{id}")
     public ResponseEntity<NoteDto> update(
             @PathVariable String id,
             @Valid @RequestBody EntryNoteDto dto,
@@ -55,4 +59,21 @@ public class NoteController {
         return ResponseEntity.status(HttpStatus.OK).body(service.updateNote(dto, id));
     }
 
+    @GetMapping()
+    public ResponseEntity<Page<NoteDto>> getNotes(
+            @Parameter(hidden = true) @PageableDefault(size = 12) Pageable pageable) {
+        return ResponseEntity.ok(service.getNotes(pageable));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteNote(
+            @Parameter(description = "Id of the Note to delete.", example = "528f22c3-1f9c-493f-8334-c70b83b5b885")
+            @PathVariable String id) {
+        if (service.existById(id)) {
+            service.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>("Nota no encontrada", HttpStatus.NOT_FOUND);
+        }
+    }
 }
