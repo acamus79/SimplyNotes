8464959dbc87b18b7a131a03460aa985d83808b1
Adrian Edgardo Camus <adrianedgardocamus@gmail.com>
2023-09-01 22:23:08 -0300

    FirstCommit

---
+++ backend/SimplyNotes/src/main/java/com/aec/simplynotes/SimplyNotesApplication.java
@@ -0,0 +1,13 @@
+package com.aec.simplynotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplyNotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimplyNotesApplication.class, args);
+    }
+
+}
---
+++ backend/SimplyNotes/src/test/java/com/aec/simplynotes/SimplyNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aec.simplynotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplyNotesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
