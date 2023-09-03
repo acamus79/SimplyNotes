0c8552102d39c3da82134369be65c8cab3978b3e
Adrian Edgardo Camus <adrianedgardocamus@gmail.com>
2023-09-03 13:26:27 -0300

    Update NoteModal.js

--- frontend/simply-notes/src/componets/NoteModal.js
+++ frontend/simply-notes/src/componets/NoteModal.js
@@ -1,5 +1,9 @@
 import React, { useState } from "react";
-import { updateNote } from "../api";
+import { updateNote, deleteNote } from "../api";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSave, faTimes, faTrashAlt, faFile } from "@fortawesome/free-solid-svg-icons";
+import { Tooltip } from "react-tooltip";
+import "./styles.css";
 
 function NoteModal({ note, onClose }) {
 
@@ -15,9 +19,22 @@ function NoteModal({ note, onClose }) {
     setEditedNote({ ...editedNote, [name]: value });
   };
 
+  const handleDeleteClick = async () => {
+    try {
+      await deleteNote(editedNote.id); // Borrar una nota
+      onClose();
+    } catch (error) {
+      console.error("Error al eliminar la nota:", error);
+    }
+  };
+
+  const handleArchiveClick = () => {
+    setEditedNote({ ...editedNote, archived: !editedNote.archived });
+  };
+
   const handleSubmit = async (event) => {
     event.preventDefault(); // Evita que el formulario se envíe automáticamente
-
+    
     try {
       // Envía la nota editada al servidor utilizando la función updateNote
       await updateNote(editedNote);
@@ -30,10 +47,18 @@ function NoteModal({ note, onClose }) {
   };
 
   return (
+    <div className="modal-overlay">
     <div className="modal">
+      <button
+            className="close-button"
+            onClick={onClose}
+          >
+            <FontAwesomeIcon icon={faTimes} />
+          </button>
       <h2>Editar Nota</h2>
       <form onSubmit={handleSubmit}>
         <label htmlFor="title">Título:</label>
+        <div className="input-container">
         <input
           type="text"
           id="title"
@@ -41,15 +66,48 @@ function NoteModal({ note, onClose }) {
           value={editedNote.title}
           onChange={handleFieldChange}
         />
+        </div>
         <label htmlFor="content">Contenido:</label>
+        <div className="input-container">
         <textarea
           id="content"
           name="content"
           value={editedNote.content}
           onChange={handleFieldChange}
         />
-        <button type="submit" onClick={send}>Guardar</button>
+        </div>
+        <div className="buttons">
+            <button
+              type="button"
+              onClick={handleDeleteClick}
+              className="btn"
+            >
+              <FontAwesomeIcon icon={faTrashAlt} className="icon delete-icon" />
+              &nbsp; Borrar
+            </button>
+            <button
+              className='btn'
+              type="button"
+              onClick={handleArchiveClick}
+              disabled={editedNote.archived}
+            >
+              <FontAwesomeIcon icon={faFile} className="icon archive-icon" />
+              &nbsp; Archivar
+            </button>
+            <button 
+              className='btn'
+              type="submit" onClick={send}
+              data-tip="Guardar" // Texto del tooltip
+              data-for="saveTooltip" // Identificador del tooltip
+            >
+              <FontAwesomeIcon icon={faSave} className="icon save-icon" />
+              &nbsp; Guardar
+            </button>
+        </div>
       </form>
+    </div>
+      <Tooltip id="saveTooltip" place="top" effect="solid" />
+      <Tooltip id="closeTooltip" place="top" effect="solid" />
     </div>
   );
 }
