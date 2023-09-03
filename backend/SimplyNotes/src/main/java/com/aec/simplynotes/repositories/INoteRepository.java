e99dc724ea49767b49a68bf98b892607f66bbf3c
Adrian Edgardo Camus <adrianedgardocamus@gmail.com>
2023-09-03 18:21:16 -0300

    FEAT:/Archived endpoint

--- backend/SimplyNotes/src/main/java/com/aec/simplynotes/repositories/INoteRepository.java
+++ backend/SimplyNotes/src/main/java/com/aec/simplynotes/repositories/INoteRepository.java
@@ -12,11 +12,19 @@ public interface INoteRepository  extends JpaRepository<NoteEntity, String> {
 
     /**
      * Method to search only the records with the softDelete field set to false,
-     *
+     * and archive field set to false
      * @return List(Entity)
      */
-    @Query("SELECT n FROM NoteEntity n WHERE n.softDelete = false")
+    @Query("SELECT n FROM NoteEntity n WHERE n.softDelete = false AND n.archived = false")
     public List<NoteEntity> searchAllNonDeleted();
+
+    /**
+     * Method to search only the records with the softDelete field set to false,
+     * and archive field set to true
+     * @return List(Entity)
+     */
+    @Query("SELECT n FROM NoteEntity n WHERE n.softDelete = false AND n.archived = true")
+    public List<NoteEntity> searchAllArchived();
 
     @Query("SELECT n FROM NoteEntity n WHERE n.user = :id AND n.softDelete = false")
     public List<NoteEntity> searchAllNonDeletedByUserId(String id);
