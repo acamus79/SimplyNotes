0155bfe2d1d6b1c85c10887725274bd5ee5e76d8
Adrian Edgardo Camus <adrianedgardocamus@gmail.com>
2023-09-08 23:01:21 -0300

    remove swagger

---
+++ backend/SimplyNotes/src/main/java/com/aec/simplynotes/controllers/UserController.java
@@ -0,0 +1,12 @@
+package com.aec.simplynotes.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class UserController {
+    
+}
